943d12c14a17fc4443165457b8c19705507f7909
Miroslav Mitov <47675969+mnmitov@users.noreply.github.com>
2020-03-26 02:31:15 +0200

    Fix issue

--- js/collapsible.js
+++ js/collapsible.js
@@ -5,10 +5,8 @@ btn.addEventListener('click', collapse, true);
 
 function collapse() {
   if (content.classList == 'content-active') {
-    console.log('first line')
     content.classList.replace('content-active', 'content');
   } else {
-    console.log('second line')
     content.classList.replace('content', 'content-active');
   }
 }
