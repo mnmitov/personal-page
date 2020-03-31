01872d2525e93615d8fe2a033cdbd51d8493e623
Miroslav Mitov <47675969+mnmitov@users.noreply.github.com>
2020-04-01 01:38:14 +0300

    Update mail form

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,12 +1,11 @@
-const btn = document.getElementById('btn');
-const content = document.getElementById('content')
-
+var btn = document.getElementById('btn');
+var content = document.getElementById('content');
 btn.addEventListener('click', collapse, true);
-
 function collapse() {
-  if (content.classList == 'content-active') {
-    content.classList.replace('content-active', 'content');
-  } else {
-    content.classList.replace('content', 'content-active');
-  }
+    if (String(content.classList) == 'content-active') {
+        content.classList.replace('content-active', 'content');
+    }
+    else {
+        content.classList.replace('content', 'content-active');
+    }
 }
